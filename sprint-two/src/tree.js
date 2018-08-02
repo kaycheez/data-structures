8ed0598d807100c33fc5efb7ea8c346c309ae8f7
kaycheez <kevin.m.chau@gmail.com>
2018-08-01 21:25:00 -0700

    working on tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,9 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
-
+  _.extend(newTree, treeMethods)
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
 
   return newTree;
 };
@@ -11,10 +11,29 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var child = Tree(value);
+  this.children.push(child);
 };
 
 treeMethods.contains = function(target) {
+  var that = this;
+  var findIt = function(target) {
+    
+    if (that.value === target) {
+      return true;
+    } else {
+        for (let i = 0; i < that.children.length; i++) {
+          return that.children[i].contains(target);
+        } 
+    } 
+  } 
+  
+  return findIt(target) || false;
+  
+// return false;
 };
+
+
 
 
 
