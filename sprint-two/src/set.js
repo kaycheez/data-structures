4a25846e794ecb96b6b8c8fa8a1b87fd25a0de07
kaycheez <kevin.m.chau@gmail.com>
2018-08-02 15:46:11 -0700

    Completed set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,28 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {}; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this._storage[item] = item;
 };
 
 setPrototype.contains = function(item) {
+  for (var key in this._storage) {
+    // if(this._storage.hasOwnProperty(key))
+    if (key === item) {
+      return true;
+    }
+  }
+  return false;
+  
 };
 
 setPrototype.remove = function(item) {
+  delete this._storage[item];
 };
 
 /*
