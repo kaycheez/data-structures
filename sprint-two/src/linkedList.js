13d21b32ac1cfb04fdd46150119a9537f0b528ac
kaycheez <kevin.m.chau@gmail.com>
2018-08-01 18:20:23 -0700

    finished linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,37 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newNode = Node(value);
+    if (list.head === null && list.tail === null) {
+      list.head = newNode;
+      list.tail = newNode;
+    } else {
+      list.tail.next = newNode;
+    // console.log("this is Node(value: " + Node(value));
+    // console.log("this is tail's next: " + list.tail.next);
+      list.tail = newNode;
+    }
+    
+    
   };
 
   list.removeHead = function() {
+    var removed = list.head.value;
+    if (list.head.next === null) {
+      list.head = null;
+    } else {
+      list.head = list.head.next;
+    }
+    return removed;
   };
 
   list.contains = function(target) {
+   for (var key in list) {
+     if (list[key].value === target) {
+       return true;
+     }
+   }
+   return false;
   };
 
   return list;
