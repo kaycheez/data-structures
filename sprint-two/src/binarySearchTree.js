dc23e1d841483c7a0b4740474bb6b30a3f122d32
kaycheez <kevin.m.chau@gmail.com>
2018-08-02 20:19:45 -0700

    working on binarySearchTree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,82 @@
 var BinarySearchTree = function(value) {
+  var binarySearchtree = Object.create(BinarySearchTreePrototype);
+  binarySearchtree.value = value;
+  binarySearchtree.left = {};
+  binarySearchtree.right = {};
+  return binarySearchtree;
 };
 
+BinarySearchTreePrototype = {};
+
+BinarySearchTreePrototype.insert = function(value) {
+  var rootNode = this;
+  var newNode = BinarySearchTree(value);
+  // var recurse = function(newNode) {
+  //   if (rootNode.value === undefined) {
+  //     rootNode.value = value;
+  //   } else if (value < rootNode.value) {
+  //     rootNode = rootNode.left;
+  //     recurse(rootNode);
+  //   }
+  // };
+  var recurse = function(newNode) {
+    if (rootNode.value > newNode.value) {
+      if (Object.keys(rootNode.left).length === 0) {
+        rootNode.left = newNode;
+      } else {
+        rootNode = rootNode.left;
+        recurse(newNode);
+      }
+    } else {
+      if (Object.keys(rootNode.right).length === 0) {
+        rootNode.right = newNode;
+      } else {
+        rootNode = rootNode.right;
+        recurse(newNode);
+      }
+    }
+  };
+  recurse(newNode);
+};
+
+BinarySearchTreePrototype.contains = function(value) {
+  var result = false;
+  var rootNode = this;
+
+  var findIt = function(value) {
+    if (rootNode.value === value) {
+      result = true;
+    } else if (rootNode.value > value && rootNode.left !== undefined) {
+      rootNode = rootNode.left;
+      findIt(value);
+    } else if (rootNode.value < value && rootNode.right !== undefined) {
+      rootNode = rootNode.right;
+      findIt(value);
+    }
+  };
+  findIt(value);
+  return result;
+};
+
+BinarySearchTreePrototype.depthFirstLog = function(func) {
+  
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
